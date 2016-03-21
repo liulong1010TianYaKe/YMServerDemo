36903f17b25a7b9d6daeeed55f29d100148ae2f6
long <long@iMac.local>
2016-03-21 19:35:17 +0800

    YM  框架

---
+++ YMServerDemo/SongInforModel.m
@@ -0,0 +1,15 @@
+//
+//  SongInforModel.m
+//  PeoPleMusic
+//
+//  Created by Alen on 16/3/5.
+//  Copyright © 2016年 kyo. All rights reserved.
+//
+
+#import "SongInforModel.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation SongInforModel
+
+
+@end
---
+++ YMServerDemo/UserInfoModel.m
@@ -0,0 +1,22 @@
+//
+//  UserInfoModel.m
+//  PeoPleMusic
+//
+//  Created by Alen on 16/3/5.
+//  Copyright © 2016年 kyo. All rights reserved.
+//
+
+#import "UserInfoModel.h"
+#import "UIDevice-Hardware.h"
+#import "NSString+IPAddress.h"
+
+@implementation UserInfoModel
++ (NSDictionary *)getUserInfoDict:(BOOL)isAdmin{
+    UserInfoModel *model = [[UserInfoModel alloc] init];
+    model.isAdmin = isAdmin;
+    model.userIp = [NSString localWiFiIPAddressAndPort];
+    model.userName = [NSString stringWithFormat:@"%@%@%@",[UIDevice currentDevice].name,[UIDevice currentDevice].systemName,[UIDevice currentDevice].systemVersion];
+    model.userId = [UIDevice getUUID];
+    return [model keyValues];
+}
+@end
